c86df7a907c226bbb8ae744a166fe8f655a5c4e3
Jprummel <jordiprummel@live.nl>
2015-12-03 14:13:39 +0100

    Map structure and Xbox controller input

---
+++ Assets/Scripts/Input/Player2.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player2 : Player2Input {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
